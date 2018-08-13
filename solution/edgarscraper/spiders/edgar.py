aeaa4984118b6a9ce1db60f514457345c377bc03
David Eads <davideads@gmail.com>
2018-08-13 01:19:58 -0500

    add some comments

--- solution/edgarscraper/spiders/edgar.py
+++ solution/edgarscraper/spiders/edgar.py
@@ -2,6 +2,12 @@ import scrapy
 
 
 class EdgarSpider(scrapy.Spider):
+    """
+    Extract company data from a local instance of Edgar.
+
+    This spider pages through the listings and follows each company link.
+    """
+
     name = "edgar"
 
     start_urls = [
@@ -12,6 +18,8 @@ class EdgarSpider(scrapy.Spider):
         # Follow every company link in the listing table
         for href in response.css('table.table a::attr(href)'):
             yield response.follow(href, self.parse_company)
+
+        # Use "next" button to reliably find next listing page.
         next_page = response.css('ul.pagination li.next:not(.disabled) a::attr(href)').extract_first()
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
