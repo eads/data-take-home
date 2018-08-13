88885d06efaee8d35a4101660f885379d98ac74f
David Eads <davideads@gmail.com>
2018-08-13 01:19:41 -0500

    follow company links

--- solution/edgarscraper/spiders/edgar.py
+++ solution/edgarscraper/spiders/edgar.py
@@ -9,6 +9,12 @@ class EdgarSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
+        # Follow every company link in the listing table
+        for href in response.css('table.table a::attr(href)'):
+            yield response.follow(href, self.parse_company)
         next_page = response.css('ul.pagination li.next:not(.disabled) a::attr(href)').extract_first()
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
+
+    def parse_company(self, response):
+        yield {}
