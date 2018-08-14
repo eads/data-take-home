88af541e2b06f5deb4358b61b43a714f2d387ecb
David Eads <davideads@gmail.com>
2018-08-14 08:25:26 -0400

    refine date parsing behavior

--- solution/csv/tests/test_normalize.py
+++ solution/csv/tests/test_normalize.py
@@ -1,5 +1,16 @@
 import normalize
 import pandas as pd
+import pytest
+
+
+DATESTRINGS = [
+    ('March 12, 2000', ('2000-03-12', None)),
+    ('July 1994', (None, 'Incomplete: July 1994')),
+    ('02/07', (None, 'Incomplete: 02/07')),
+    ('Impedit eum ipsam.', (None, 'Invalid: Impedit eum ipsam.')),
+    ('04/09/1970', ('1970-04-09', None)),
+    ('1970-09-05', ('1970-09-05', None)),
+]
 
 df = pd.read_csv('../../files/data.csv')
 state_abbreviation_crosswalk = pd.read_csv('../../files/state_abbreviations.csv', index_col='state_abbr')
@@ -16,21 +27,6 @@ def test_clean_bio():
     assert test_df.ix[2].to_dict()['bio'] == 'Sed vitae dolorem quae totam sequi fuga odit. Eaque alias quisquam blanditiis veniam. Aut perferendis sint deleniti accusamus quod.'
 
 
-def test_clean_partial_date():
-    test_df = df.copy()
-    test_df['start_date'], test_df['start_date_description'] = normalize.clean_dates(test_df)
-    assert test_df.ix[0].to_dict()['start_date'] == '2006-10-01'
-    assert test_df.ix[0].to_dict()['start_date_description'] is None
-
-
-def test_clean_bad_date():
-    test_df = df.copy()
-    test_df['start_date'], test_df['start_date_description'] = normalize.clean_dates(test_df)
-    assert test_df.ix[1].to_dict()['start_date'] is None
-    assert test_df.ix[1].to_dict()['start_date_description'] == 'Voluptatem odio.'
-
-def test_clean_good_date():
-    test_df = df.copy()
-    test_df['start_date'], test_df['start_date_description'] = normalize.clean_dates(test_df)
-    assert test_df.ix[7].to_dict()['start_date'] == '1994-10-20'
-    assert test_df.ix[7].to_dict()['start_date_description'] is None
+@pytest.mark.parametrize("datestring,expected", DATESTRINGS)
+def test_clean_dates(datestring, expected):
+    assert normalize._clean_dates(datestring) == expected
